8c220b3919e548766162fd0664f1526ab0cfdec7
Alex <jabba-qwin@yandex.ru>
2024-11-24 15:07:14 +0300

    Вариант кода с заглушками (домашнее задание 2)

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Не нажимай кнопку \"Настройки\"", Toast.LENGTH_SHORT).show()
+            }
         }
+        searchButton.setOnClickListener(searchButtonClickListener)
+        mediaButton.setOnClickListener(this)
+        settingsButton.setOnClickListener(this)
+
+    }
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.media_button -> {
+                Toast.makeText(this, "Смотри значение кнопки выше", Toast.LENGTH_SHORT).show()
+            }
+            R.id.settings_button -> {
+                Toast.makeText(this, "Ну вот и зачем, спрашивается?", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
